60c5a4a4537e8ec213b16f8d31d73a527d0c5ed4
Ayham-Abbar <ayham.abbar11@gmail.com>
2025-01-21 18:30:02 +0300

    تجريبي

--- resources/views/dashboard/master.blade.php
+++ resources/views/dashboard/master.blade.php
@@ -1,64 +1,67 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
- @include('partials.head')
- @yield('title')
+    @include('partials.head')
+    @yield('title')
 
 </head>
 
 
 <body class="bg-theme bg-theme1">
-@include('section.allert')
+    @include('section.allert')
 
-<!-- Start wrapper-->
- <div id="wrapper">
+    <!-- Start wrapper-->
+    <div id="wrapper">
 
-  <!--Start sidebar-wrapper-->
-  @yield('sidebar')
+        <!--Start sidebar-wrapper-->
+        @yield('sidebar')
 
-   <!--End sidebar-wrapper-->
+        <!--End sidebar-wrapper-->
 
-<!--Start topbar header-->
-@include('partials.header')
-<!--End topbar header-->
+        <!--Start topbar header-->
+        @include('partials.header')
+        <!--End topbar header-->
 
-<div class="clearfix"></div>
+        <div class="clearfix"></div>
 
-  <div class="content-wrapper">
-    <div class="container-fluid">
+        <div class="content-wrapper">
+            <div class="container-fluid">
 
-  <!--Start Dashboard Content-->
-
-       @yield('content')
-
-      <!--End Dashboard Content-->
-
-	<!--start overlay-->
-		  <div class="overlay toggle-menu"></div>
-		  <!--end overlay-->
-
-    </div>
-    <!-- End container-fluid-->
-
-    </div>
-    <!--End content-wrapper-->
-   <!--Start Back To Top Button-->
-   @include('partials.backToTopButten')
-    <!--End Back To Top Button-->
-
-	<!--Start footer-->
-	@include('partials.footer')
-	<!--End footer-->
-
-  <!--start color switcher-->
-  @include('partials.colorSwitcher')
-  <!--end color switcher-->
-
-  </div><!--End wrapper-->
+                <!--Start Dashboard Content-->
 
 
-  <!-- Bootstrap core JavaScript-->
-  @include('partials.js')
+                @yield('content')
+
+                <!--End Dashboard Content-->
+
+                <!--start overlay-->
+                <div class="overlay toggle-menu"></div>
+                <!--end overlay-->
+
+            </div>
+            <!-- End container-fluid-->
+
+        </div>
+        <!--End content-wrapper-->
+        <!--Start Back To Top Button-->
+        @include('partials.backToTopButten')
+        <!--End Back To Top Button-->
+
+        <!--Start footer-->
+        @include('partials.footer')
+        <!--End footer-->
+
+        <!--start color switcher-->
+        @include('partials.colorSwitcher')
+        <!--end color switcher-->
+
+    </div><!--End wrapper-->
+
+
+    <!-- Bootstrap core JavaScript-->
+    @include('partials.js')
 
 </body>
+
 </html>
