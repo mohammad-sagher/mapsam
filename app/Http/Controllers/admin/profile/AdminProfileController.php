f999163f05c177aa0c22f4d4c05308ac7d6d3e8f
mohammad-sagher <mohammadssagher@gmail.com>
2025-01-22 18:26:58 +0300

    controller profile

---
+++ app/Http/Controllers/admin/profile/AdminProfileController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\admin\profile;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Profile;
+use App\Http\Requests\DataProfileActiveRequest;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Support\Facades\Hash;
+
+class AdminProfileController extends Controller
+{
+    //
+    public static function ShowProfile(){
+        $admin=Auth::guard('admin')->user();
+        $profile=Profile::where('id',$admin->profile_id)->first();
+        return view('admin.profile.show',compact('profile'));
+    }
+
+    public function updateActivity(DataProfileActiveRequest $request){
+        $admin=Auth::guard('admin')->user();
+        $profile=Profile::where('id',$admin->profile_id)->first();
+        $profile->update([
+            'Recent_badges' => $request->Recent_badges,
+            'recent_activity' => $request->recent_activity,
+            'hobbies' => $request->hobbies,
+            'about' => $request->about,
+        ]);
+        return redirect()->route('admin.profile.show')->with('success', 'Profile updated successfully');
+
+
+
+    }
+
+
+    public function updateData(Request $request){
+
+
+
+        $admin=Auth::guard('admin')->user();
+        $profile=Profile::where('id',$admin->profile_id)->first();
+        $profile->update([
+            'username' => $request->username,
+            'phone' => $request->phone,
+            'address' => $request->address,
+
+        ]);
+
+        if($request->hasFile('image')){
+            $image = $request->file('image');
+            $imageName = time().'.'.$image->getClientOriginalExtension();
+            $image->move(public_path('images/profile'), $imageName);
+           $profile->update([
+            'image' => $imageName,
+           ]);
+        }
+
+        return redirect()->route('admin.profile.show')->with('success', 'Profile updated successfully');
+    }
+
+
+    public function updatePassword(Request $request){
+        $validated = $request->validateWithBag('updatePassword', [
+            'password' => ['required', Password::defaults(), 'confirmed'],
+        ]);
+        $admin=Auth::guard('admin')->user();
+        $admin->update([
+            'password' => Hash::make($validated['password']),
+        ]);
+        return redirect()->route('admin.profile.show')->with('success', 'Password updated successfully');
+    }
+}
+
