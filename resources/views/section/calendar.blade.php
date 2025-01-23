ba0fab7e0e5170a178d2649fb2572011525cdb96
mohammad-sagher <mohammadssagher@gmail.com>
2025-01-23 23:20:33 +0300

    formatting

--- resources/views/section/calendar.blade.php
+++ resources/views/section/calendar.blade.php
@@ -1,81 +1,67 @@
 @extends('dashboard.master')
+
 @section('title')
     Admin
 @endsection
+
 @section('sidebar')
     @include('partials.sidebarAdmin')
-
-
 @endsection
 
 @section('content')
-<div class="clearfix"></div>
+    <div class="clearfix"></div>
 
-  <div class="content-wrapper">
-    <div class="container-fluid">
+    <div class="content-wrapper">
+        <div class="container-fluid">
 
-    <div class="mt-3">
-      <div id='calendar'></div>
-    </div>
+            <div class="mt-3">
+                <div id='calendar'></div>
+            </div>
 
-		<!--start overlay-->
-		  <div class="overlay toggle-menu"></div>
-		<!--end overlay-->
+            <!--start overlay-->
+            <div class="overlay toggle-menu"></div>
+            <!--end overlay-->
 
-    </div>
-    <!-- End container-fluid-->
-   </div><!--End content-wrapper-->
+            <!--start color switcher-->
+            <div class="right-sidebar">
+                <div class="switcher-icon">
+                    <i class="zmdi zmdi-settings zmdi-hc-spin"></i>
+                </div>
+                <div class="right-sidebar-content">
 
-  <!--Start Back To Top Button-->
-    <a href="javaScript:void();" class="back-to-top"><i class="fa fa-angle-double-up"></i> </a>
-    <!--End Back To Top Button-->
+                    <p class="mb-0">Gaussion Texture</p>
+                    <hr>
 
-	<!--Start footer-->
-	<footer class="footer">
-      <div class="container">
-        <div class="text-center">
-          Copyright © 2018 Dashtreme Admin
+                    <ul class="switcher">
+                        <li id="theme1"></li>
+                        <li id="theme2"></li>
+                        <li id="theme3"></li>
+                        <li id="theme4"></li>
+                        <li id="theme5"></li>
+                        <li id="theme6"></li>
+                    </ul>
+
+                    <p class="mb-0">Gradient Background</p>
+                    <hr>
+
+                    <ul class="switcher">
+                        <li id="theme7"></li>
+                        <li id="theme8"></li>
+                        <li id="theme9"></li>
+                        <li id="theme10"></li>
+                        <li id="theme11"></li>
+                        <li id="theme12"></li>
+                        <li id="theme13"></li>
+                        <li id="theme14"></li>
+                        <li id="theme15"></li>
+                    </ul>
+
+                </div>
+            </div>
+            <!--end color switcher-->
+
         </div>
-      </div>
-    </footer>
-	<!--End footer-->
-
-	<!--start color switcher-->
-   <div class="right-sidebar">
-    <div class="switcher-icon">
-      <i class="zmdi zmdi-settings zmdi-hc-spin"></i>
+        <!-- End container-fluid-->
     </div>
-    <div class="right-sidebar-content">
-
-      <p class="mb-0">Gaussion Texture</p>
-      <hr>
-
-      <ul class="switcher">
-        <li id="theme1"></li>
-        <li id="theme2"></li>
-        <li id="theme3"></li>
-        <li id="theme4"></li>
-        <li id="theme5"></li>
-        <li id="theme6"></li>
-      </ul>
-
-      <p class="mb-0">Gradient Background</p>
-      <hr>
-
-      <ul class="switcher">
-        <li id="theme7"></li>
-        <li id="theme8"></li>
-        <li id="theme9"></li>
-        <li id="theme10"></li>
-        <li id="theme11"></li>
-        <li id="theme12"></li>
-		<li id="theme13"></li>
-        <li id="theme14"></li>
-        <li id="theme15"></li>
-      </ul>
-
-     </div>
-   </div>
-  <!--end color switcher-->
+    <!--End content-wrapper-->
 @endsection
-
