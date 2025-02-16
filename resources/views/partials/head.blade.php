407acfb4375ad3650a30886b800b334980c97090
mohammad-sagher <mohammadssagher@gmail.com>
2025-02-16 14:06:27 +0300

    include js

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -6,7 +6,8 @@
     <meta name="author" content=""/>
     <title>Dashtreme Admin - Free Dashboard for Bootstrap 4 by Codervent</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('assets/js/app-script.js') }}"></script>
     <!-- loader-->
     <link href="{{ asset('assets/css/pace.min.css') }}" rel="stylesheet"/>
     <script src="{{ asset('assets/js/pace.min.js') }}"></script>
--- resources/views/partials/sidebarDoctor.blade.php
+++ resources/views/partials/sidebarDoctor.blade.php
@@ -1,75 +1,54 @@
 <!--Start sidebar-wrapper-->
-    <div id="sidebar-wrapper" data-simplebar="" data-simplebar-auto-hide="true">
-      <div class="brand-logo">
-       <a href="index.html">
-        <img src="{{asset('assets/images/logo-icon.png') }}" class="logo-icon" alt="logo icon">
-        <h5 class="logo-text">Dashtreme Admin</h5>
-      </a>
+<div id="sidebar-wrapper" data-simplebar="" data-simplebar-auto-hide="true">
+    <div class="brand-logo">
+        <a href="index.html">
+            <img src="{{ asset('assets/images/logo-icon.png') }}" class="logo-icon" alt="logo icon">
+            <h5 class="logo-text">Dashtreme Admin</h5>
+        </a>
     </div>
     <ul class="sidebar-menu do-nicescrol">
-       <li class="sidebar-header">MAIN NAVIGATION</li>
-       <li>
-         <a href="{{route('doctor.dashboard')}}">
-           <i class="zmdi zmdi-view-dashboard"></i> <span>Dashboard</span>
-         </a>
-       </li>
+        <li class="sidebar-header">MAIN NAVIGATION</li>
+        <li>
+            <a href="{{ route('doctor.dashboard') }}">
+                <i class="zmdi zmdi-view-dashboard"></i> <span>Dashboard</span>
+            </a>
+        </li>
 
-       <li>
+        <!-- Available Times Section -->
+        <li class="nav-item">
+            <a href="#" class="nav-link" id="availableTimesToggle">
+                <i class="zmdi zmdi-invert-colors"></i> <span>Available Times</span>
+                <i class="zmdi zmdi-chevron-down float-end"></i> <!-- Arrow icon -->
+            </a>
+            <ul class="submenu" id="availableTimesSubmenu">
+                <li><a class="text-white dropdown-item" href="{{ route('doctor.avaliable_times.create') }}">Create Available Time</a></li>
+                <li><a class="text-white dropdown-item" href="{{ route('doctor.avaliable_times.index') }}">Show Available Times</a></li>
+            </ul>
+        </li>
 
+        <!-- Other Menu Items -->
+        <li>
+            <a href="{{ route('doctor.profile') }}">
+                <i class="zmdi zmdi-face"></i> <span>Profile</span>
+            </a>
+        </li>
 
-      </li>
-
-       <li>
-         <a href="{{route('icones')}}">
-           <i class="zmdi zmdi-invert-colors"></i> <span>UI Icons</span>
-         </a>
-       </li>
-
-       <li>
-         <a href="{{route('forms')}}">
-           <i class="zmdi zmdi-format-list-bulleted"></i> <span>Forms</span>
-         </a>
-       </li>
-
-       <li>
-         <a href="{{route('tables')}}">
-           <i class="zmdi zmdi-grid"></i> <span>Tables</span>
-         </a>
-       </li>
-
-       <li>
-         <a href="{{route('calendar')}}">
-           <i class="zmdi zmdi-calendar-check"></i> <span>Calendar</span>
-           <small class="float-right badge badge-light">New</small>
-         </a>
-       </li>
-
-       <li>
-         <a href="{{route('doctor.profile')}}">
-           <i class="zmdi zmdi-face"></i> <span>Profile</span>
-         </a>
-       </li>
-
-       <li>
-        @if(auth()->guard('doctor')->check())
-
-            <form action="{{ route('doctor.logout') }}" method="POST" style="display: inline;">
-                @csrf
-                <button type="submit" style="all: unset; cursor: pointer; color: inherit;">
-                    <i class="zmdi zmdi-lock"></i> <span>Logout doctor</span>
-                </button>
-
-            </form>
+        <li>
+            @if(auth()->guard('doctor')->check())
+                <form action="{{ route('doctor.logout') }}" method="POST" style="display: inline;">
+                    @csrf
+                    <button type="submit" style="all: unset; cursor: pointer; color: inherit;">
+                        <i class="zmdi zmdi-lock"></i> <span>Logout doctor</span>
+                    </button>
+                </form>
             @endif
-    </li>
+        </li>
+
+        <li class="sidebar-header">LABELS</li>
+        <li><a href="javaScript:void();"><i class="zmdi zmdi-coffee text-danger"></i> <span>Important</span></a></li>
+        <li><a href="javaScript:void();"><i class="zmdi zmdi-chart-donut text-success"></i> <span>Warning</span></a></li>
+        <li><a href="javaScript:void();"><i class="zmdi zmdi-share text-info"></i> <span>Information</span></a></li>
+    </ul>
+</div>
 
 
-       <li class="sidebar-header">LABELS</li>
-       <li><a href="javaScript:void();"><i class="zmdi zmdi-coffee text-danger"></i> <span>Important</span></a></li>
-       <li><a href="javaScript:void();"><i class="zmdi zmdi-chart-donut text-success"></i> <span>Warning</span></a></li>
-       <li><a href="javaScript:void();"><i class="zmdi zmdi-share text-info"></i> <span>Information</span></a></li>
-
-     </ul>
-
-    </div>
-    <!--End sidebar-wrapper-->!
